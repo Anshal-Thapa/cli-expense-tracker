0e5a0540b4d597d3f070e687d6032b3341a5391f
Anshal Thapa <anshal.thapa@bajratechnologies.com>
2026-09-02 11:29:29 +0545

    fix: path of json file outside of project

--- cli_expense_tracker/storage.py
+++ cli_expense_tracker/storage.py
@@ -5,12 +5,12 @@ def save_expenses(expenses):
     json_data = []
     for each_expense in expenses:
         json_data.append(each_expense.to_dict())
-    with open("../expenses.json","w",encoding="UTF-8") as f:
+    with open("./expenses.json","w",encoding="UTF-8") as f:
         json.dump(json_data,f)
 
 def load_expenses():
     try:
-        with open("../expenses.json","r",encoding="UTF-8") as f:
+        with open("./expenses.json","r",encoding="UTF-8") as f:
             json_data = json.load(f)
     except FileNotFoundError:
         return []
